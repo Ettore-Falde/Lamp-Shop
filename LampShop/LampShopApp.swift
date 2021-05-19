20c5eaf91d107003954400097a9ebc97dcc8b436
Ettore Falde <ettorefalde97@gmail.com>
2021-05-19 14:38:41 +0200

    Initial Commit

---
+++ LampShop/LampShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LampShopApp.swift
+//  LampShop
+//
+//  Created by Ettore Falde on 19/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LampShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
